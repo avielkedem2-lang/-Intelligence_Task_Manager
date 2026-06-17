10e9ff9b8e23765430f0885a6879cd6718105ac3
אביאל קדם <avielkedem2@gmail.com>
2026-06-17 17:28:06 +0300

    add 3 methods get_mission_by_id, assign_mission, update_mission_status into class Missions

--- database/mission_db.py
+++ database/mission_db.py
@@ -9,10 +9,10 @@ class MissionDB:
 
     def create_mission(self,data):
         try:
-            data["risk_level"] = data["difficulty"] *2 + data["importance"]
+            # data["risk_level"] = data["difficulty"] *2 + data["importance"]
             self.db.get_connection()
-            self.db.cursor.execute("INSERT INTO missions (title, description, location, difficulty, importance, risk_level) VALUES (%s, %s, %s, %s, %s, %s)",
-                                   (data["title"], data["description"], data["location"], data["difficulty"], data["importance"], data["risk_level"]))
+            self.db.cursor.execute("INSERT INTO missions (title, description, location, difficulty, importance, risk_level) VALUES (%s, %s, %s, %s, %s)",
+                                   (data["title"], data["description"], data["location"], data["difficulty"], data["importance"]))
             self.db.connection.commit()
             return self.db.cursor.fetchone()
         except Exception as e:
@@ -30,4 +30,43 @@ class MissionDB:
             print(e)
         finally:
             self.db.cursor.close()
+    
+
+    def get_mission_by_id(self, id):
+        try:
+            self.db.get_connection()
+            self.db.cursor.execute("select * from missions where id=%s", (id,))
+            return self.db.cursor.fetchone()
+        except Exception as e:
+            print(e)
+        finally:
+            self.db.cursor.close()
+    
+
+    def assign_mission(self, m_id, a_id):
+        try:
+            self.db.get_connection()
+            self.db.cursor.execute("UPDATE missions SET assigned_agent_id=%s WHERE id=%s",
+                                   (m_id, a_id))
+            self.db.connection.commit()
+            return True
+        except Exception as e:
+            print(e)
+        finally:
+            self.db.cursor.close()
+    
+
+
+    def update_mission_status(self, id, status):
+        try:
+            self.db.get_connection()
+            self.db.cursor.execute("UPDATE missions SET status=%s WHERE id=%s",
+                                   (status, id))
+            self.db.connection.commit()
+            return True
+        except Exception as e:
+            print(e)
+        finally:
+            self.db.cursor.close()
+
         
